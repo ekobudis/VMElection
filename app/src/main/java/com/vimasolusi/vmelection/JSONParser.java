13e11853394bf3fe2bf2de6bbfb8fa5b913b158e
caca <eko.budi2283@gmail.com>
2019-03-22 16:29:03 +0700

    Add TPS for API

--- app/src/main/java/com/vimasolusi/vmelection/JSONParser.java
+++ app/src/main/java/com/vimasolusi/vmelection/JSONParser.java
@@ -29,7 +29,7 @@ public class JSONParser {
     }
     // function get json from url
     // by making HTTP POST or GET mehtod
-    public JSONObject   makeHttpRequest(String url, String method, List<NameValuePair> params) {
+    public JSONObject  makeHttpRequest(String url, String method, List<NameValuePair> params) {
         // Making HTTP request
         try {
             // check for request method
